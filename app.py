e15719cddd241da473dba803ea4980bb873291ee
asifnawazminhas <asifnawazminhasnl@gmail.com>
2025-07-24 17:54:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return '''
+    Hello, World!<br><br>
+    Subdomain takeover by 
+    <a href="https://www.asifnawazminhas.com/" target="_blank">asifnawazminhas.com</a> and 
+    <a href="https://www.linkedin.com/in/asifminhasnl/" target="_blank">LinkedIn</a>
+    '''
